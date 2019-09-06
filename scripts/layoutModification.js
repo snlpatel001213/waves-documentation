75df0dd7c6aedb27c99f873c70106b0eca971477
sbibik <sbibik@waveplatform.com>
2019-09-06 14:57:26 +0300

    change width content body of layout

--- scripts/layoutModification.js
+++ scripts/layoutModification.js
@@ -2,5 +2,5 @@ jQuery(document).ready(function () {
   var $ = jQuery;
   var element = $('.page-inner')[0];
   element.style.maxWidth = '1440px';
-  element.style.padding = '20px 10%';
+  element.style.padding = '20px 10% 40px 10%';
 });
